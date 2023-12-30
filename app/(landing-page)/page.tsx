49675e73fef6b6afcb33618a4786db243fd9556c
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2023-12-30 18:57:09 +0100

    build: seventh section

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/ui/navbar/navbar";
-import React from "react";
-import FirstSection from "./_components/first-section";
 import Sliders from "./_components/slider/sliders";
+import FirstSection from "./_components/first-section";
 import SecondSection from "./_components/second-section";
 import ThirdSection from "./_components/third-section";
 import FourthSection from "./_components/fourth-section";
+import FifthSection from "./_components/fifth-section";
+import SixthSection from "./_components/sixth-section";
+import SeventhSection from "./_components/seventh-section";
 
 const LandingPage = () => {
   return (
@@ -15,6 +17,9 @@ const LandingPage = () => {
       <SecondSection />
       <ThirdSection />
       <FourthSection />
+      <FifthSection />
+      <SixthSection />
+      <SeventhSection />
     </div>
   );
 };
