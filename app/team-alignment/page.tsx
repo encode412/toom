e53f2f3e9b8873d46930410a46a803646d3e6a15
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2024-01-01 06:28:17 +0100

    build: team alignment page

---
+++ app/team-alignment/page.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/ui/navbar/navbar"
+import FirstSection from "./_components/first-section"
+import SecondSection from "./_components/second-section"
+
+const TeamAlignment = () => {
+  return (
+    <div>
+        <Navbar />
+        <FirstSection />
+        <SecondSection />
+        
+        </div>
+  )
+}
+
+export default TeamAlignment
