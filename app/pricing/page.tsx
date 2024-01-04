a6394337403da93166554cab68d6faebed985930
encodedevv <timothy.onyeacholam@nxghubdigitech.onmicrosoft.com>
2024-01-05 00:55:44 +0100

    fix: page naming corrected

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/ui/navbar/navbar";
 import FirstSection from "./_components/first-section";
-import Testimonials from "../toom-ai/_components/testimonials";
+import Testimonials from "../Llum-ai/_components/testimonials";
 import BottomCard from "@/components/ui/bottom-card";
 import BottomSection from "@/components/ui/bottom-section";
 import Footer from "@/components/ui/footer";
